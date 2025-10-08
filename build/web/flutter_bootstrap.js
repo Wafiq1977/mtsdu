5d2e93056f097e4279babe3a2936c979f427a2e8
Wafiq1977 <mochwafiqizna@gmail.com>
2025-10-08 14:03:09 +0700

    rebuild web with correct base href

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"a8bfdfc394deaed5c57bd45a64ac4294dc976a
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2962696168"
+    serviceWorkerVersion: "660908068"
   }
 });
