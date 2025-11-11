51d43ad2b746be5a61b33cca4654dec855d8cb1a
Wafiq1977 <mochwafiqizna@gmail.com>
2025-11-11 21:42:32 +0700

    Force add Flutter Web build

--- android/app/src/main/kotlin/com/example/lpmmtsdu/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.lpmmtsdu
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- lib/models/base_model.dart
+++
@@ -1,9 +0,0 @@
-abstract class BaseModel {
-  String id;
-
-  BaseModel({required this.id});
-
-  Map<String, dynamic> toMap();
-
-  // fromMap is usually factory in subclasses, so not defined here
-}
